530187cb29a91df35b55e54bed80310b6c0199cf
Forever <brianpigs1717@gmail.com>
2026-02-08 17:29:53 -0500

    install.sh fixes
    Updated install to be more convenient

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo cp ./script/* /usr/bin
-echo "Installation complete."
+sudo cp ./script/* /bin #Copy to bin
+rm -rf ./script/ # Delete scripts after being copied
+echo "Installation complete. You can now delete this file."
