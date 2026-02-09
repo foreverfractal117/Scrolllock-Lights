247282bc61b39e50a6eabd59f6b163dd88b2c154
Forever <brianpigs1717@gmail.com>
2026-02-09 16:19:30 -0500

    improve wording
    yes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo cp ./script/* /bin #Copy to bin
 rm -rf ./script/ # Delete scripts after being copied
-echo "Installation complete. You can now delete this file."
+echo "Installation complete. You can now delete this file and directory."
